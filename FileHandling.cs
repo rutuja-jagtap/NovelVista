c0987b176619542d4f93bbc32a9b8020a996048c
Jagtap Rutuja Vijay <70133640+rutuja-jagtap@users.noreply.github.com>
2022-04-01 10:46:22 +0530

    File handling c#

---
+++ FileHandling.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileHandlingProject
+{
+    internal class FileHandling
+    {
+        public void ReadFile()
+        {
+            FileStream fileStream = new FileStream(@"C:\SqlSession\SqlHandsOn\Rutuja.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fileStream);
+            Console.WriteLine("ID\t\tSource\t\tDestination\t\tDate\t\tTime\t\tStatus\t\tNetwork");
+            while (reader.Peek() > 0)
+            {
+                var Id = reader.ReadLine();
+                string[] netIdStr = Id.Split(':');
+                int id = Convert.ToInt32(netIdStr[1]);
+
+                var source = reader.ReadLine();
+                string[] sourceStr = source.Split(':');
+                string src = sourceStr[1];
+
+                var dest = reader.ReadLine();
+                string[] desStr = dest.Split(':');
+                string des = desStr[1];
+
+                var time = reader.ReadLine();
+                string[] tStr = time.Split(' ');
+                string times = tStr[1];
+                string mode = tStr[2];
+                string[] tStr1 = tStr[0].Split(':');
+                string date = tStr1[1];
+
+                var stat = reader.ReadLine();
+                string[] statStr = stat.Split(':');
+                string status = statStr[1];
+
+                var net = reader.ReadLine();
+                string[] nettStr = net.Split(':');
+                string network = nettStr[1];
+
+                //for (id >0)
+                {
+                    if (status.Contains("Failed"))
+                        Console.WriteLine(id + "\t\t" + src + "\t" + des + "\t\t" + date + "\t" + times + "\t" + mode + "\t" + status + "\t\t" + network);
+
+                }
+
+
+                reader.ReadLine();
+
+            }
+
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileHandlingProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            FileHandling fileHandling = new FileHandling();
+            //fileHandling.WriteFile();
+            fileHandling.ReadFile();
+            Console.Read();
+        }
+    }
+}
